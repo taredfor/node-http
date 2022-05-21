85e11a90eaafc2ebdd79f741a38cd56933e94857
taredfor <taredfor@localhost>
2022-05-21 12:10:26 +0300

    add new build

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import http from 'node:http';
 
 const server = http.createServer((req, res) => {
     res.writeHead (200);
-    res.end( 'node ok');
+    res.end( 'ok');
 });
 
-server.listen( 9999);
+server.listen( 7777);
